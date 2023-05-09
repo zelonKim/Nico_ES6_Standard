1002d12946536e43aa20aa679f076f86be8966fa
zelonKim <ksz18601@gmail.com>
2023-05-09 14:51:34 +0900

    'this'in Arrow Functions

--- FUNCTIONS/app.js
+++ FUNCTIONS/app.js
@@ -1,97 +1,64 @@
-/*
-const names = ['nico','seongjin','lynn']
+/* const button = document.querySelector("button")
 
-const hearts = names.map(function(item) { 
-    return item + "♥";
-});
-
-console.log(hearts); // ['nico♥', 'seongjin♥', 'lynn♥']
-*/
-
-/*
-const names = ['nico','seongjin','lynn']
-
-function addHeart(item) {
-    return item + "♥";
-}
-
-const hearts = names.map(addHeart);
-
-console.log(hearts); // ['nico♥', 'seongjin♥', 'lynn♥']
-*/
-
-/*
-const names = ['nico','seongjin','lynn']
-
-function addHeart(item) {
-    return 0;
-}
-
-const hearts = names.map(addHeart);
-
-console.log(hearts); // [0, 0, 0]
-*/
-
-/*
-const names = ['nico','seongjin','lynn']
-
-const hearts = names.map((item) => { 
-    return item + "♥";
-});
-
-console.log(hearts); // ['nico♥', 'seongjin♥', 'lynn♥']
-*/
-
-/*
-const names = ['nico','seongjin','lynn']
-
-const hearts = names.map((item, index) => { 
-    console.log("we are on", index)
-    return item + "♥";
-});
-
-console.log(hearts); 
-// we are on 0
-// we are on 1
-// we are on 2
-// ['nico♥', 'seongjin♥', 'lynn♥']
-*/
-
-/*
-const names = ['nico','seongjin','lynn']
-
-const hearts = names.map( item => item + "♥");
-
-console.log(hearts); // ['nico♥', 'seongjin♥', 'lynn♥']
-*/
-
-/*
-const names = ['nico','seongjin','lynn']
-
-const hearts = names.map( item => 0 );
-
-console.log(hearts); // [0, 0, 0]
-*/
-
-/*
-const names = ['nico','seongjin','lynn']
-
-const hearts = names.map( item => { item + "♥"} );
-
-console.log(hearts);
+button.addEventListener("click", function() {
+    console.log(this);}
+);  // 버튼을 클릭하면 콘솔 창에 <button>hello</button> 가 표시됨.
 */
 
 /* 
-const names = ['nico','seongjin','lynn']
+const button = document.querySelector("button")
 
-const hearts = names.map( (item, index) => item + "♥" + index );
-
-console.log(hearts); // ['nico♥0', 'seongjin♥1', 'lynn♥2']
+button.addEventListener("click", function() {
+    this.style.backgroundColor = "red";}
+);  // 버튼을 클릭하면 버튼이 빨간색으로 바뀜.
 */
 
 
-const names = ['nico','seongjin','lynn']
 
-const hearts = names.map( () => "♥" );
 
-console.log(hearts) // ['♥', '♥', '♥']
+/* 
+const button = document.querySelector("button")
+
+button.addEventListener("click", () => { // 화살표 함수로 변환
+    console.log(this);}
+);  // 버튼을 클릭하면 콘솔 창에 window객체가 표시됨.
+*/
+
+
+/* 
+const button = document.querySelector("button")
+
+button.addEventListener("click", () => { 
+    this.style.backgroundColor = "red";}
+);  // 버튼을 클릭하면 오류가 발생함.
+*/
+
+
+
+/* const nico = {
+    name: "seongjin",
+    age: 28,
+    addYear: function() {
+        this.age++; // this가 nico 객체를 가리킴.
+    }
+};
+console.log(nico); // {name: 'seongjin', age: 28, addYear: ƒ} 
+
+nico.addYear();
+console.log(nico); // {name: 'seongjin', age: 29, addYear: ƒ} // age가 1 증가함.
+*/
+
+
+
+
+const nico = {
+    name: "seongjin",
+    age: 28,
+    addYear: () => { // 화살표 함수로 변환
+        this.age++;  // this가 Window객체를 가리킴.
+    }
+};
+console.log(nico); // {name: 'seongjin', age: 28, addYear: ƒ}
+
+nico.addYear();
+console.log(nico); // {name: 'seongjin', age: 28, addYear: ƒ}  // age가 그대로임. 
